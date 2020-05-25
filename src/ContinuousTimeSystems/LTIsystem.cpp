42b03badb4512f2dd9c6e2a0c0e610205f6a2dc4
longhaao qian <longhao.qian@mail.utoronto.ca>
2020-05-25 17:19:31 -0400

    modified the readme file

--- src/ContinuousTimeSystems/LTIsystem.cpp
+++ src/ContinuousTimeSystems/LTIsystem.cpp
@@ -1,111 +1,112 @@
 #include "pch.h"
 #include "LTIsystem.h"
+namespace linearsystem {
+	LTIsystem::LTIsystem()
+	{}
 
-LTIsystem::LTIsystem()
-{}
+	LTIsystem::LTIsystem(const LTIParameter& parameter, const LTIInitialCondition& IC)
+	{
+		state = IC.X_0;// assign the initial condition to the state
+		system_info.type = continous_LTI;
+		A = parameter.A;
+		B = parameter.B;
+		C = parameter.C;
+		D = parameter.D;
+		// determine whether D is a zero matrix
+		if (D.norm() < 0.0000000001)// very small norm
+		{
+			system_info.DIRECT_FEED_THROUGH = false;
+		}
+		else {
+			system_info.DIRECT_FEED_THROUGH = true;
+		}
+		// determine the size of the system
+		system_info.num_of_continuous_states = A.rows();
+		system_info.num_of_inputs = B.cols();
+		system_info.num_of_outputs = C.rows();
+		// check the compatibility of the system matrices:
+		system_info.system_parameter_ok = true;
+		if (A.rows() != B.rows())
+		{
+			system_info.system_parameter_ok = false;
+		}
 
-LTIsystem::LTIsystem(const LTIParameter& parameter, const LTIInitialCondition& IC)
-{
-	state = IC.X_0;// assign the initial condition to the state
-	system_info.type = continous_LTI;
-	A = parameter.A;
-	B = parameter.B;
-	C = parameter.C;
-	D = parameter.D;
-	// determine whether D is a zero matrix
-	if (D.norm() < 0.0000000001)// very small norm
-	{
-		system_info.DIRECT_FEED_THROUGH = false;
-	}
-	else {
-		system_info.DIRECT_FEED_THROUGH = true;
-	}
-	// determine the size of the system
-	system_info.num_of_continuous_states = A.rows();
-	system_info.num_of_inputs = B.cols();
-	system_info.num_of_outputs = C.rows();
-	// check the compatibility of the system matrices:
-	system_info.system_parameter_ok = true;
-	if (A.rows() != B.rows())
-	{
-		system_info.system_parameter_ok = false;
+		if (A.rows() != A.cols())
+		{
+			system_info.system_parameter_ok = false;
+		}
+
+		if (A.rows() != C.cols())
+		{
+			system_info.system_parameter_ok = false;
+		}
+
+		if (C.rows() != D.rows())
+		{
+			system_info.system_parameter_ok = false;
+		}
+
+		if (system_info.num_of_inputs != D.cols())
+		{
+			system_info.system_parameter_ok = false;
+		}
+
+		if (system_info.system_parameter_ok == true)
+		{
+			ready_to_run = true;
+			// initialize state memeory
+			state.resize(system_info.num_of_continuous_states);
+			output.resize(system_info.num_of_outputs);
+			system_info.input_connection.resize(system_info.num_of_inputs, 2);
+		}
+		else {
+			ready_to_run = false;
+		}
+
+
 	}
 
-	if (A.rows() != A.cols())
+
+	LTIsystem::~LTIsystem()
 	{
-		system_info.system_parameter_ok = false;
 	}
 
-	if (A.rows() !=  C.cols())
+
+	void LTIsystem::DifferentialEquation(const double& t, const VectorXd& state, const VectorXd& input, VectorXd& derivative)
 	{
-		system_info.system_parameter_ok = false;
+		// x_dot  = A x + B u
+		derivative = A * state + B * input;
 	}
 
-	if (C.rows() != D.rows())
+	void LTIsystem::OutputEquation(const double& t, const VectorXd& state, const VectorXd& input, VectorXd& output)
 	{
-		system_info.system_parameter_ok = false;
+		// y = Cx + Du
+		output = C * state + D * input;
 	}
 
-	if (system_info.num_of_inputs != D.cols())
+	void LTIsystem::IncrementState()
 	{
-		system_info.system_parameter_ok = false;
+		state += solver_buffer_state_increment1;
+	}
+	void LTIsystem::DisplayParameters()
+	{
+		cout << "---------------------" << endl;
+		cout << "LTI system parameter:" << endl;
+		cout << "A = " << endl << A << endl;
+		cout << "----" << endl;
+		cout << "B = " << endl << B << endl;
+		cout << "----" << endl;
+		cout << "C = " << endl << C << endl;
+		cout << "----" << endl;
+		cout << "D = " << endl << D << endl;
 	}
 
-	if (system_info.system_parameter_ok == true)
+	void LTIsystem::DisplayInitialCondition()
 	{
-		ready_to_run = true;
-		// initialize state memeory
-		state.resize(system_info.num_of_continuous_states);
-		output.resize(system_info.num_of_outputs);
-		system_info.input_connection.resize(system_info.num_of_inputs, 2);
+		cout << "---------------------" << endl;
+		cout << "LTI system initial condition X_0:" << endl;
+		cout << "The initial condition is:  " << endl;
+		cout << state << endl;
 	}
-	else {
-		ready_to_run = false;
-	}
-	
 
 }
-
-
-LTIsystem::~LTIsystem()
-{
-}
-
-
-void LTIsystem::DifferentialEquation(const double& t, const VectorXd& state, const VectorXd& input, VectorXd& derivative)
-{
-	// x_dot  = A x + B u
-	derivative = A * state + B * input;
-}
-
-void LTIsystem::OutputEquation(const double& t, const VectorXd& state, const VectorXd& input, VectorXd& output)
-{
-	// y = Cx + Du
-	output = C * state + D * input;
-}
-
-void LTIsystem::IncrementState()
-{
-	state += solver_buffer_state_increment1;
-}
-void LTIsystem::DisplayParameters()
-{
-	cout << "---------------------" << endl;
-	cout<< "LTI system parameter:"<<endl;
-	cout << "A = " << endl << A << endl;
-	cout << "----" << endl;
-	cout<< "B = " <<endl<< B << endl;
-	cout << "----" << endl;
-	cout<< "C = " <<endl << C << endl;
-	cout << "----" << endl;
-	cout<< "D = " <<endl<< D << endl;
-}
-
-void LTIsystem::DisplayInitialCondition()
-{
-	cout << "---------------------" << endl;
-	cout << "LTI system initial condition X_0:" << endl;
-	cout << "The initial condition is:  " << endl;
-	cout << state << endl;
-}
-
--- src/ContinuousTimeSystems/LTIsystem.h
+++ src/ContinuousTimeSystems/LTIsystem.h
@@ -1,34 +1,77 @@
 #pragma once
 #include "Subsystem.h"
-#define READYTOGO 0;
-#define STATE_INPUT_MISMATCH 1;
-#define STATE_OUTPUT_MISMATCH 2;
-#define INPUT_OUTPUT_MISMATCH 3;
-struct LTIParameter{
-	MatrixXd A;
-	MatrixXd B;
-	MatrixXd C; 
-	MatrixXd D;
-};
 
-struct LTIInitialCondition {
-	VectorXd X_0;
-};
-using namespace std;
-class LTIsystem :
-	public Subsystem
-{
-private:
-	// parameters
-	MatrixXd A, B, C, D;
-public:
-	LTIsystem();
-	LTIsystem(const LTIParameter& parameter, const LTIInitialCondition& IC);
-	void DifferentialEquation(const double& t, const VectorXd& state, const VectorXd& input, VectorXd& derivative);
-	void OutputEquation(const double& t, const VectorXd& state, const VectorXd& input, VectorXd& output);
-	void IncrementState();
-	void DisplayParameters();
-	void DisplayInitialCondition();
-	~LTIsystem();
-};
+namespace linearsystem {
+	struct LTIParameter{
+		MatrixXd A;
+		MatrixXd B;
+		MatrixXd C; 
+		MatrixXd D;
+	};
+	struct LTIInitialCondition {
+		VectorXd X_0;
+	};
+	using namespace std;
+	class LTIsystem :
+		public Subsystem
+	{
+	private:
+		// parameters
+		MatrixXd A, B, C, D;
+	public:
+		LTIsystem();
+		LTIsystem(const LTIParameter& parameter, const LTIInitialCondition& IC);
+		void DifferentialEquation(const double& t, const VectorXd& state, const VectorXd& input, VectorXd& derivative);
+		void OutputEquation(const double& t, const VectorXd& state, const VectorXd& input, VectorXd& output);
+		void IncrementState();
+		void DisplayParameters();
+		void DisplayInitialCondition();
+		~LTIsystem();
+	};
+
+	struct IntegratorParameter {
+		int num_of_channels;
+	};
+	struct IntegratorInitialCondition {
+		VectorXd X_0;
+	};
+	class Integrator :
+		public Subsystem
+	{
+	private:
+		IntegratorParameter parameter;
+	public:
+		Integrator();
+		Integrator(const IntegratorParameter& parameter, const IntegratorInitialCondition& IC);
+		void DifferentialEquation(const double& t, const VectorXd& state, const VectorXd& input, VectorXd& derivative);
+		void OutputEquation(const double& t, const VectorXd& state, const VectorXd& input, VectorXd& output);
+		void IncrementState();
+		void DisplayParameters();
+		void DisplayInitialCondition();
+		~Integrator();
+	};
+
+	struct TransferFunctionParameter {
+		VectorXd Numerator;
+		VectorXd Denominator;
+	};
+	class TransferFunction :
+		public Subsystem
+	{
+	private:
+		TransferFunctionParameter parameter;
+		MatrixXd A, B, C, D;
+	public:
+		TransferFunction();
+		TransferFunction(const TransferFunctionParameter& parameter);
+		void DifferentialEquation(const double& t, const VectorXd& state, const VectorXd& input, VectorXd& derivative);
+		void OutputEquation(const double& t, const VectorXd& state, const VectorXd& input, VectorXd& output);
+		void IncrementState();
+		void DisplayParameters();
+		void DisplayInitialCondition();
+		~TransferFunction();
+	};
+}
+
+
 
